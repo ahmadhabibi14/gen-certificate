4118292f25dfbf2cc21e9ad7c695d42c51347b03
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-06-19 20:55:44 +0800

    [fix] precision

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource/inter/100.css';
+	import '@fontsource/inter/200.css';
+	import '@fontsource/inter/300.css';
+	import '@fontsource/inter/400.css';
+	import '@fontsource/inter/500.css';
+	import '@fontsource/inter/600.css';
+	import '@fontsource/inter/700.css';
+	import '@fontsource/inter/800.css';
+	import '@fontsource/inter/900.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>Generate Certificate</title>
+</svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
   <div class="container mx-auto my-10 flex flex-col justify-center items-center gap-5">
     <h1 class="text-4xl font-bold">Generate Certificate</h1>
     {#if isCertificateReady}
-      <div bind:this={certificateEl} id="certificate" class="certificate aspect-auto border border-neutral-200 relative">
+      <div bind:this={certificateEl} id="certificate" class="certificate">
         <img
           src="img/certificate-template.png"
           alt="template"
@@ -34,31 +34,71 @@
         />
 
         <!-- Label Nama -->
-        <div class="absolute text-xl top-60 left-26">
-          <span class="text-dark-cert">Diberikan kepada</span>
-        </div>
+        <span class="label-nama">Diberikan kepada</span>
         <!-- Nama Lengkap -->
-        <div class="absolute text-5xl font-bold top-66 left-26">
-          <span class="text-uniqhbaunderground">Ahmad Rizky Nusantara Habibi</span>
-        </div>
+        <span class="nama-lengkap">Ahmad Rizky Nusantara Habibi</span>
 
         <!-- Label Pencapaian -->
-        <div class="absolute text-xl top-87 left-26">
-          <span class="text-dark-cert">Atas partisipasi pada acara</span>
-        </div>
+        <span class="label-achievement">Atas partisipasi pada acara</span>
         <!-- Pencapaian -->
-        <div class="absolute text-5xl font-bold top-95 left-26 w-7/12 leading-14">
-          <span class="text-uniqhbaunderground">Meet Up Cyber Security: The Smart Way to Start in Cyber Security</span>
+        <div class="achievement">
+          <p>Meet Up Cyber Security: The Smart Way to Start in Cyber Security</p>
         </div>
 
         <!-- Tanggal Sertifikat -->
-        <div class="absolute text-xl bottom-27 left-171">
-          <span class="text-dark-cert font-bold">14 Juni 2025</span>
+        <span class="date">14 Juni 2025</span>
+
+        <div class="ttd">
+          <!-- DEKAN -->
+          <div class="item">
+            <div class="img-container">
+              <img
+                src="/img/ttd-dekan.png"
+                alt=""
+              />
+            </div>
+            <div class="name-container">
+              <span class="nama">Muhammad Yamin S.T, M.T</span>
+              <span class="jabatan">Dekan Fakultas Sains dan Teknologi UNIQHBA</span>
+            </div>
+          </div>
+
+          <!-- KETUA KOMUNITAS -->
+          <div class="item">
+            <div class="img-container">
+              <img
+                src="/img/ttd-ketua-komunitas.png"
+                alt=""
+              />
+            </div>
+            <div class="name-container">
+              <span class="nama">Ahmad Rizky Nusantara Habibi</span>
+              <span class="jabatan">Ketua komunitas UNIQHBA Underground</span>
+            </div>
+          </div>
+
+          <!-- KETUA PANITIA -->
+          <div class="item">
+            <div class="img-container">
+              <img
+                src="/img/ttd-ketua-panitia.png"
+                alt=""
+              />
+            </div>
+            <div class="name-container">
+              <span class="nama">Sabardi Bahari</span>
+              <span class="jabatan">Ketua panitia penyelenggara</span>
+            </div>
+          </div>
         </div>
 
-        <!-- ID Sertifikat -->
-        <div class="absolute text-xl bottom-23 right-27">
-          <span class="text-dark-cert font-bold">ID: 49DK2MAPNBQP</span>
+        <div class="kredensial">
+          <div class="certificate-type">
+            <span class="field">CYBER SECURITY</span>
+            <span class="type">Certificate Completion</span>
+          </div>
+          <!-- ID Sertifikat -->
+          <span class="certificate-id">ID: 49DK2MAPNBQP</span>
         </div>
       </div>
     {/if}
@@ -69,3 +109,147 @@
     </div>
   </div>
 </div>
+
+<style>
+  :root {
+    --color-uniqhbaunderground: #075F46;
+    --color-uniqhbaunderground-2: #178E3B;
+    --color-dark-cert: #111827;
+    --color-gold: #816208;
+  }
+
+  /* CONTAINER */
+  .certificate {
+    position: relative;
+    width: 2000px;
+    height: 1211px;
+    border: 1px solid var(--color-dark-cert);
+  }
+
+  /* NAMA LENGKAP */
+  .certificate .label-nama {
+    position: absolute;
+    top: 280px;
+    left: 138px;
+    font-size: 35px;
+    color: var(--color-dark-cert);
+  }
+  .certificate .nama-lengkap {
+    position: absolute;
+    top: 325px;
+    left: 138px;
+    font-size: 50px;
+    color: var(--color-uniqhbaunderground);
+    font-weight: 700;
+  }
+
+  /* PENCAPAIAN */
+  .certificate .label-achievement {
+    position: absolute;
+    top: 430px;
+    left: 138px;
+    font-size: 35px;
+    color: var(--color-dark-cert);
+  }
+  .certificate .achievement {
+    position: absolute;
+    top: 480px;
+    left: 138px;
+    font-size: 50px;
+    color: var(--color-uniqhbaunderground);
+    font-weight: 700;
+    width: 50%;
+  }
+  .certificate .achievement p {
+    line-height: 120%;
+  }
+
+  /* TANGGAL */
+  .certificate .date {
+    position: absolute;
+    top: 690px;
+    left: 138px;
+    font-size: 35px;
+    font-weight: 700;
+    color: var(--color-dark-cert);
+  }
+
+  /* DEKAN */
+  .certificate .ttd {
+    height: fit-content;
+    position: absolute;
+    bottom: 120px;
+    left: 142px;
+    display: flex;
+    flex-direction: row;
+    gap: 80px;
+  }
+  .certificate .ttd .item {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .certificate .ttd .item .img-container {
+    width: 214px;
+    height: auto;
+  }
+
+  .certificate .ttd .item .img-container img {
+    width: 100%;
+    height: 100%;
+  }
+  .certificate .ttd .item .name-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+    color: var(--color-dark-cert);
+  }
+  .certificate .ttd .item .name-container .nama {
+    font-size: 30px;
+    font-weight: 600;
+  }
+  .certificate .ttd .item .name-container .jabatan {
+    font-size: 20px;
+  }
+
+  /* KREDENSIAL */
+  .certificate .kredensial {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 574px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .certificate .kredensial .certificate-type {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0;
+    justify-content: center;
+    color: var(--color-gold);
+    margin-top: 125px;
+  }
+
+  .certificate .kredensial .certificate-type .field {
+    font-weight: 700;
+    font-size: 30px;
+  }
+  .certificate .kredensial .certificate-type .type {
+    font-weight: 500;
+    font-size: 20px;
+  }
+  
+  .certificate .kredensial .certificate-id {
+    font-weight: 700;
+    font-size: 25px;
+    color: var(--color-dark-cert);
+    margin-top: 340px;
+    position: absolute;
+  }
+</style>
